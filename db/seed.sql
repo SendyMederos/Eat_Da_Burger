74a279634ecd030f118762a9f7595a646e4baf73
SendyMederos <sendymederos@hotmail.com>
2020-10-15 15:19:40 -0400

    created schema.sql, seed.sql, and connection.js

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO burger (burger_name)
+VALUES ("Cheese Burger");
+INSERT INTO burger (burger_name)
+VALUES ("Impossible Burger");
+INSERT INTO burger (burger_name)
+VALUES ("B&B Burger");
+INSERT INTO burger (burger_name)
+VALUES ("Turkey Burger");
+INSERT INTO burger (burger_name)
+VALUES ("Double Cheese Burger");
